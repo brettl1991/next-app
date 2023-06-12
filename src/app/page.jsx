cfe504af2c44da51bdb0fa9ac0c670a881917570
brettl1991 <brettl1991@gmail.com>
2023-06-12 15:17:06 +0100

    feat: create home page and style

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.description}>
+          Turning your Idea into Reality. We bring together the team from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}></div>
+      <Image src={Hero} alt="hero-image" className={styles.img} />
+    </div>
+  );
 }
