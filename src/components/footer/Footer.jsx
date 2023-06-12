a15795c0605419847508a024159d86e76fd4d66d
brettl1991 <brettl1991@gmail.com>
2023-06-12 15:40:04 +0100

    feat: add footer social icons and style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,43 @@
 import React from "react";
-import styles from "./footer.module.css#";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>&copy;2023 Lamaweb. All rights reserved.</div>
-      <div></div>
+      <div>
+        <div className={styles.socials}>
+          <Image
+            src="/1.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="Lama Dev Facebook Account"
+          />
+          <Image
+            src="/2.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="Lama Dev Instagram Account"
+          />
+          <Image
+            src="/3.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="Lama Dev Twitter Account"
+          />
+          <Image
+            src="/4.png"
+            width={15}
+            height={15}
+            className={styles.icon}
+            alt="Lama Dev YouTube Account"
+          />
+        </div>
+      </div>
     </div>
   );
 };
